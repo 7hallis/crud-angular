b4230ef8911f91a84a6b13b16d3b85c7c18a2cc2
7hallis <thallisffc@hotmail.com>
2023-06-20 21:15:42 -0300

    Adicionando Coluna Ações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -21,6 +21,21 @@ import { ProductCreateComponent } from './components/product/product-create/prod
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
+import { HttpClientModule } from '@angular/common/http'
+
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+// import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort'
+
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData} from'@angular/common'
+registerLocaleData(localePt)
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +45,9 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     HomeComponent,
     ProductCrudComponent,
     WhiteDirective,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent,
+    // ProductRead2Component
   ],
   imports: [
     BrowserModule,
@@ -41,9 +58,19 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductService } from '../product.service';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -9,13 +10,21 @@ import { ProductService } from '../product.service';
 })
 export class ProductCreateComponent {
 
+  product: Product = {
+    name: '',
+    price: null
+
+  }
   constructor(private productService: ProductService, private router: Router) {}
 
   // ngOnInit() : void {
   // }
   
   createProduct() : void {
-    this.productService.showMessage('Produto criado!')
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado!')
+      this.router.navigate(['/products'])
+    })
   }
   cancel() : void {
     this.router.navigate(['/products'])
